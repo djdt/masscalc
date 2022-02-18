b2161cf83c978024db092c8cee610a28fdc0cd20
djdt <10266332+djdt@users.noreply.github.com>
2022-02-18 13:20:12 +1100

    adducts and losses

--- masscalc/__main__.py
+++ masscalc/__main__.py
@@ -10,11 +10,18 @@ from masscalc.masscalc import calculate_masses_and_ratios, sum_unique_masses_and
 def parse_args(argv: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("formula")
-    parser.add_argument("--plot", action="store_true")
+    parser.add_argument("--species", help="Adduct or loss as M<+-><formula>.")
     parser.add_argument("--charge", type=int, default=1)
+    parser.add_argument("--plot", action="store_true")
     parser.add_argument("--minimum_isotope_abundance", type=float, default=1e-6)
     parser.add_argument("--minimum_formula_abundance", type=float, default=1e-6)
     args = parser.parse_args(argv)
+
+    m = re.match("M([+-])([A-Z][A-Za-z0-9]*)?", args.species)
+    if m is None:
+        parser.error("--species must have the form M<+-> or M<+-><formula>.")
+    args.species = m[2], 1 if m[1] == "+" else -1
+
     return args
 
 
@@ -31,6 +38,14 @@ def main():
     dict = {}
     for (s, n) in re.findall("([A-Z][a-z]?)([0-9]*)", args.formula):
         dict[s] = dict.get(s, 0) + int(n or 1)
+
+    if args.species:  # Adduct or loss
+        for (s, n) in re.findall("([A-Z][a-z]?)([0-9]*)", args.species[0]):
+            dict[s] = dict.get(s, 0) + int(n or 1) * args.species[1]
+
+    for k, v in dict.items():
+        if v < 0:
+            raise ValueError(f"Invalid number of {k}, {k} = {v}.")
 
     masses, ratios = calculate_masses_and_ratios(
         dict,
