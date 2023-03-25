00c2b78a560f700b09df088356e716205a52dd5d
djdt <10266332+djdt@users.noreply.github.com>
2023-03-26 09:21:21 +1100

    numpy >1.22

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name="masscalc",
-    version="0.1",
+    version="0.2",
     description="Cacluator for isotopic abundances.",
     packages=["masscalc"],
     license="LGPL",
     author="djdt",
     install_requires=[
-        "numpy",
+        "numpy>=1.22",
     ],
     entry_points={"console_scripts": ["masscalc=masscalc.__main__:main"]},
 )
